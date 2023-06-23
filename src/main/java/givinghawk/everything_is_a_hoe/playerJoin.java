e1059eb46aa466c90cffa11540589c7391d05251
givinghawk <giveinghawk@gmail.com>
2023-06-23 12:08:57 +0000

    modified:   src/main/java/givinghawk/everything_is_a_hoe/playerJoin.java

--- src/main/java/givinghawk/everything_is_a_hoe/playerJoin.java
+++ src/main/java/givinghawk/everything_is_a_hoe/playerJoin.java
@@ -9,8 +9,6 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
-import org.bukkit.dispatchCommand;
-import org.bukkit.server;
 
 public class playerJoin implements Listener {
     @EventHandler
@@ -18,9 +16,9 @@ public class playerJoin implements Listener {
         Player player = e.getPlayer();
         //Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), "clear "+player.getName());
         e.getPlayer().getInventory().clear();
-        //player.getInventory().addItem(new ItemStack(Material.WOODEN_HOE,9999999));
+        player.getInventory().addItem(new ItemStack(Material.WOODEN_HOE,9999999));
         e.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 99999, 5));
-        Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), "give "+player.getName()+" golden_hoe{display:{Name:\"[{\\\"text\\\":\\\"Most useful item\\\",\\\"italic\\\":\\\"false\\\"}]\",Lore:[\"⠀⠀⠀⣀⣠⣤⣤⣤⣀⣀⣀⡀⠀⠀⠀⠀⠀⠀⣀⣤⣤⣦⠦⠶⠴⠦⣤⣀⠀\",\"⣠⠖⢛⡉⠥⠤⠬⠌⣉⠉⠛⠋⠀⠀⠀⠀⠐⠛⠋⣉⡀⠤⠤⡤⠄⣀⠀⠁⠁\",\"⣀⣠⣵⢿⣿⣿⣿⣿⣶⢧⣄⠀⠀⠀⠀⠀⠀⢀⣬⣷⣾⣿⣿⣿⡿⣦⣄⢀⠀\",\"⠈⠻⣧⡘⣿⣿⣿⣶⡿⢈⣇⠀⠀⠀⠀⠀⠀⣐⣏⠻⣿⣿⣿⡾⢃⣼⠟⠉⠀\",\"⠀⠀⠈⠹⠶⢿⡿⠷⠾⠿⠁⠀⠀⠀⠀⠀⠀⠉⠹⠹⠾⠿⠷⠶⠏⠀⠀⠀⠀\",\"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\",\"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\",\"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\",\"⠀⠀⠀⠀⠀⠀⣠⣄⡤⣤⡖⣬⢥⣂⣔⡬⣅⣆⢤⢀⣀⠀⠀⠀⠀⠀⠀⠀⠀\",\"⠀⠀⠀⠀⠀⠀⠀⠘⢝⡷⢯⣧⣯⣾⣥⣯⣽⢾⡿⡿⠃⠀⠀⠀⠀⠀⠀⠀⠀\",\"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠋⢝⡲⢭⠭⡭⣍⣞⣣⠝⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\",\"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠁⠀⠉⠘⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\"]}} 36");
+        //Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), "give "+player.getName()+" golden_hoe{display:{Name:\"[{\\\"text\\\":\\\"Most useful item\\\",\\\"italic\\\":\\\"false\\\"}]\",Lore:[\"⠀⠀⠀⣀⣠⣤⣤⣤⣀⣀⣀⡀⠀⠀⠀⠀⠀⠀⣀⣤⣤⣦⠦⠶⠴⠦⣤⣀⠀\",\"⣠⠖⢛⡉⠥⠤⠬⠌⣉⠉⠛⠋⠀⠀⠀⠀⠐⠛⠋⣉⡀⠤⠤⡤⠄⣀⠀⠁⠁\",\"⣀⣠⣵⢿⣿⣿⣿⣿⣶⢧⣄⠀⠀⠀⠀⠀⠀⢀⣬⣷⣾⣿⣿⣿⡿⣦⣄⢀⠀\",\"⠈⠻⣧⡘⣿⣿⣿⣶⡿⢈⣇⠀⠀⠀⠀⠀⠀⣐⣏⠻⣿⣿⣿⡾⢃⣼⠟⠉⠀\",\"⠀⠀⠈⠹⠶⢿⡿⠷⠾⠿⠁⠀⠀⠀⠀⠀⠀⠉⠹⠹⠾⠿⠷⠶⠏⠀⠀⠀⠀\",\"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\",\"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\",\"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\",\"⠀⠀⠀⠀⠀⠀⣠⣄⡤⣤⡖⣬⢥⣂⣔⡬⣅⣆⢤⢀⣀⠀⠀⠀⠀⠀⠀⠀⠀\",\"⠀⠀⠀⠀⠀⠀⠀⠘⢝⡷⢯⣧⣯⣾⣥⣯⣽⢾⡿⡿⠃⠀⠀⠀⠀⠀⠀⠀⠀\",\"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠋⢝⡲⢭⠭⡭⣍⣞⣣⠝⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\",\"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠁⠀⠉⠘⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\"]}} 36");
         player.sendMessage("Woah, these are so useful! Enjoy them!");
     }
 }
